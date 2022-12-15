330726928ff6d9b4b6f6a031b2391cf39679ce41
Elisio <racabral11@icloud.com>
2022-12-15 21:01:01 +0000

    second commit
    advanced made in creating a database using SQFLITE,
    functionality for adding new coqueiro(user), updating and deleting the user, and when in coqueiro page all users are retrieved from database and displayed in a ListView.

--- lib/database/Database.dart
+++ lib/database/Database.dart
@@ -1,47 +1,134 @@
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-import 'coqueiro_model.dart';
+import 'package:sqflite/sqflite.dart' as sql;
 
-class DataBase {
-  Future<Database> initializedDB() async {
-    String path = await getDatabasesPath();
-    return openDatabase(
-      join(path, 'planets.db'),
+
+class DatabaseHelper{
+  static Future<void> createTables(sql.Database database) async{
+    await database.execute('''CREATE TABLE coqueiros(
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      firstName Text NOT NULL,
+      lastName TEXT NOT NULL,
+      dateAdded TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    )
+    
+    ''');
+  }
+
+  // initializing the database
+  static Future<sql.Database> db() async{
+    return sql.openDatabase(
+      'pingo.db',
       version: 1,
-      onCreate: (Database db, int version) async {
-        await db.execute(
-          "CREATE TABLE coqueiros(id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT NOT NULL,lastName INTEGER NOT NULL)",
-        );
+      onCreate: (sql.Database database, int version) async{
+        await createTables(database);
       },
     );
   }
 
-// insert data
-  Future<int> insertCoqueiro(List<Coqueiros> coqueiros) async {
-    int result = 0;
-    final Database db = await initializedDB();
-    for (var coqueiro in coqueiros) {
-      result = await db.insert('coqueiros', coqueiro.toMap(),
-          conflictAlgorithm: ConflictAlgorithm.replace);
-    }
+  // creating a coqueiro
+  static Future<int> createCoqueiro(String? firstName, String? lastName) async{
+    final db = await DatabaseHelper.db();
+    final data = {'firstName':firstName, 'lastName':lastName};
+    final id = await db.insert('coqueiros', data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace);
 
+    return id;
+  }
+
+  // getting all coqueiro
+  static Future<List<Map<String, dynamic>>> getCoqueiros() async{
+    final db = await DatabaseHelper.db();
+    return db.query('coqueiros', orderBy: "id");
+  }
+
+  // getting single coqueiro
+  static Future<List<Map<String, dynamic>>> getSingleCoqueiro(int id) async{
+    final db = await DatabaseHelper.db();
+    return db.query('coqueiros',where: "id = ?",whereArgs: [id], limit: 1);
+  }
+
+  // updating coqueiro detail
+ static Future<int> updateCoqueiro(
+     int id, String firstName, String lastName
+     ) async{
+    final db = await DatabaseHelper.db();
+
+    final data ={
+      'firstName': firstName,
+      'lastName':lastName,
+      'dateAdded': DateTime.now().toString()
+    };
+
+    final result = await db.update('coqueiros', data, where: "id = ?",
+        whereArgs: [id]);
     return result;
-  }
+ }
 
-// retrieve data
-  Future<List<Coqueiros>> retrieveCoqueiros() async {
-    final Database db = await initializedDB();
-    final List<Map<String, Object?>> queryResult = await db.query('coqueiros');
-    return queryResult.map((e) => Coqueiros.fromMap(e)).toList();
-  }
+ // deleting a coqueiro
+ static Future<void> deleteCoqueiro(int id) async{
+    final db = await DatabaseHelper.db();
 
-// delete user
-  Future<void> deleteCoqueiro(int id) async {
-    final db = await initializedDB();
-    await db.delete(
-      'coqueiros',
-      where: "id = ?",
-      whereArgs: [id],
-    );
-  }
+    try{
+      await db.delete('coqueiros', where: "id=?", whereArgs: [id]);
+    }catch(err){
+      debugPrint("something went wrong when deleting an item: $err");
+    }
+ }
+
+
+
 }
+
+
+
+
+
+
+
+// class DataBase {
+//   Future<Database> initializedDB() async {
+//     String path = await getDatabasesPath();
+//     return openDatabase(
+//       join(path, 'planets.db'),
+//       version: 1,
+//       onCreate: (Database db, int version) async {
+//         await db.execute(
+//           "CREATE TABLE coqueiros(id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT NOT NULL,lastName INTEGER NOT NULL)",
+//         );
+//       },
+//     );
+//   }
+//
+// // insert data
+//   Future<int> insertCoqueiro(List<Coqueiros> coqueiros) async {
+//     int result = 0;
+//     final Database db = await initializedDB();
+//     for (var coqueiro in coqueiros) {
+//       result = await db.insert('coqueiros', coqueiro.toMap(),
+//           conflictAlgorithm: ConflictAlgorithm.replace);
+//     }
+//
+//     return result;
+//   }
+//
+// // retrieve data
+//   Future<List<Coqueiros>> retrieveCoqueiros() async {
+//     final Database db = await initializedDB();
+//     final List<Map<String, Object?>> queryResult = await db.query('coqueiros');
+//     return queryResult.map((e) => Coqueiros.fromMap(e)).toList();
+//   }
+//
+// // delete user
+//   Future<void> deleteCoqueiro(int id) async {
+//     final db = await initializedDB();
+//     await db.delete(
+//       'coqueiros',
+//       where: "id = ?",
+//       whereArgs: [id],
+//     );
+//   }
+// }
+
+
+
--- lib/pages/coqueiro_page.dart
+++ lib/pages/coqueiro_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pingu_di_mel/database/coqueiro_model.dart';
-
 import '../database/Database.dart';
 
 class CoqueiroList extends StatefulWidget {
@@ -11,30 +9,123 @@ class CoqueiroList extends StatefulWidget {
 }
 
 class _CoqueiroListState extends State<CoqueiroList> {
+  List<Map<String, dynamic>> _journals= [];
 
-  late DataBase handler;
-  Future<int> addCoqueiros() async {
-    Coqueiros firstPlanet =
-    Coqueiros(id: 1,firstName: "Elisio", lastName: "Sa");
-    Coqueiros secondPlanet =
-    Coqueiros(id: 2,firstName: "Ju", lastName: "Sanca");
-    Coqueiros thirdPlanet =
-    Coqueiros(id: 3, firstName: "Urien", lastName: "Santos");
-    Coqueiros fourthPlanet =
-    Coqueiros(id: 4, firstName: "Kevin", lastName: "Sovetas");
+  bool _isLoading = true;
 
-    List<Coqueiros> coqueiros = [firstPlanet, secondPlanet,thirdPlanet,fourthPlanet];
-    return await handler.insertCoqueiro(coqueiros);
+  void _refreshJournals() async{
+    final data = await DatabaseHelper.getCoqueiros();
+    setState(() {
+      _journals = data;
+      _isLoading = false;
+    });
   }
+
+  // late DataBase handler;
+  // Future<int> addCoqueiros() async {
+  //   Coqueiros firstPlanet =
+  //   Coqueiros(id: 1,firstName: "Elisio", lastName: "Sa");
+  //   Coqueiros secondPlanet =
+  //   Coqueiros(id: 2,firstName: "Ju", lastName: "Sanca");
+  //   Coqueiros thirdPlanet =
+  //   Coqueiros(id: 3, firstName: "Urien", lastName: "Santos");
+  //   Coqueiros fourthPlanet =
+  //   Coqueiros(id: 4, firstName: "Kevin", lastName: "Sovetas");
+  //
+  //   List<Coqueiros> coqueiros = [firstPlanet, secondPlanet,thirdPlanet,fourthPlanet];
+  //   return await handler.insertCoqueiro(coqueiros);
+  // }
 
   @override
   void initState() {
     super.initState();
-    handler = DataBase();
-    handler.initializedDB().whenComplete(() async {
-      await addCoqueiros();
-      setState(() {});
-    });
+    _refreshJournals();
+    print('...number of coqueiros: ${_journals.length}');
+  }
+
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+
+
+  Future<void> _addCoqueiro() async{
+    await DatabaseHelper.createCoqueiro(
+        _firstNameController.text, _lastNameController.text);
+    _refreshJournals();
+    print('...number of coqueiros: ${_journals.length}');
+  }
+
+  Future<void> _updateCoqueiro(int id) async{
+    await DatabaseHelper.updateCoqueiro(id,
+        _firstNameController.text, _lastNameController.text);
+    _refreshJournals();
+  }
+
+
+  // delete coqueiro
+  void _deleteCoqueiro(int id ) async{
+    await DatabaseHelper.deleteCoqueiro(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Item deleted successfully'),
+    ));
+    _refreshJournals();
+  }
+
+  void _showForm(int? id) async {
+    if(id != null){
+      final existingJournal =
+          _journals.firstWhere((element) => element['id']==id);
+      _firstNameController.text = existingJournal['firstName'];
+      _lastNameController.text = existingJournal['lastName'];
+    }
+
+    showModalBottomSheet(
+        context: context,
+        elevation: 5,
+        isScrollControlled: true,
+        builder: (_)=> Container(
+          padding: EdgeInsets.only(
+            top: 15,
+            left: 15,
+            right: 15,
+            // this prevent the soft keybord from covering text field
+            bottom: MediaQuery.of(context).viewInsets.bottom +400,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                controller: _firstNameController,
+                decoration: const InputDecoration(hintText: 'First Name'),
+              ),
+              TextField(
+                controller: _lastNameController,
+                decoration: const InputDecoration(hintText: 'Last name'),
+              ),
+              const SizedBox(
+            height: 10,
+        ),
+              ElevatedButton(
+                  onPressed: () async{
+                    if(id == null){
+                      await _addCoqueiro();
+                    }
+
+                    if(id != null){
+                      await _updateCoqueiro(id);
+                    }
+                    // clear the text fields
+                    _firstNameController.text = '';
+                    _lastNameController.text = '';
+                    // close the bottom sheet
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(id == null? 'Create New' : 'Update'),
+              )
+              
+            ],
+          ),
+        ));
   }
 
 
@@ -43,28 +134,37 @@ class _CoqueiroListState extends State<CoqueiroList> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: FutureBuilder(
-        future: handler.retrieveCoqueiros(),
-        builder:
-            (BuildContext context, AsyncSnapshot<List<Coqueiros>> snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: ListTile(
-                    contentPadding: const EdgeInsets.all(8.0),
-                    title: Text(snapshot.data![index].firstName),
-                    subtitle: Text(snapshot.data![index].id.toString()),
-                  ),
-                );
-              },
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: ()=>_showForm(null),
       ),
+      body: ListView.builder(
+          itemCount: _journals.length,
+          itemBuilder: (context, index)=> Card(
+            color: Colors.grey,
+            margin: const EdgeInsets.all(5),
+            child: ListTile(
+              leading: Text(_journals[index]['dateAdded']),
+              title: Text(_journals[index]['firstName']),
+              subtitle: Text(_journals[index]['lastName']),
+              trailing: SizedBox(
+                width: 100,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: ()=>_showForm(_journals[index]['id']),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: ()=>_deleteCoqueiro(_journals[index]['id']),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+      )
     );
   }
 }
